0bbe4e49cae505f91fec54abdc1b32bbf691b889
徐智勇 <bostonhsu@outlook.com>
2017-06-04 15:17:39 +0800

    另外一种多线程安全的实现。利用了synchronized 关键字。
    缺点：性能可能下降到100倍。

--- src/cn/windssoft/singleton/ChocolateBoiler.java
+++ src/cn/windssoft/singleton/ChocolateBoiler.java
@@ -7,14 +7,17 @@ public class ChocolateBoiler {
     private boolean empty;
     private boolean boiled;
 
-    private static ChocolateBoiler uniqueChocolateBoiler = new ChocolateBoiler();
+    private static ChocolateBoiler uniqueChocolateBoiler;
 
     private ChocolateBoiler() {
         empty = true;
         boiled = false;
     }
 
-    public static ChocolateBoiler getInstance() {
+    public static synchronized ChocolateBoiler getInstance() {
+        if (uniqueChocolateBoiler == null) {
+            uniqueChocolateBoiler = new ChocolateBoiler();
+        }
         return uniqueChocolateBoiler;
     }
 
