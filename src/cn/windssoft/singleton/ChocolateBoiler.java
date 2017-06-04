a0b63424ab066d070ad442644e5bb62756330137
徐智勇 <bostonhsu@outlook.com>
2017-06-04 15:14:41 +0800

    创建了单例例子。但是ChocolateBoiler 类是个有多线程隐患的单例类。

---
+++ src/cn/windssoft/singleton/ChocolateBoiler.java
@@ -0,0 +1,51 @@
+package cn.windssoft.singleton;
+
+/**
+ * Created by Boston Hsu on 6/4/2017.
+ */
+public class ChocolateBoiler {
+    private boolean empty;
+    private boolean boiled;
+
+    private static ChocolateBoiler uniqueChocolateBoiler;
+
+    private ChocolateBoiler() {
+        empty = true;
+        boiled = false;
+    }
+
+    public static ChocolateBoiler getInstance() {
+        if (uniqueChocolateBoiler == null) {
+            uniqueChocolateBoiler = new ChocolateBoiler();
+        }
+        return uniqueChocolateBoiler;
+    }
+
+    public void fill() {
+        if (isEmpty()) {
+            empty = false;
+            boiled = false;
+        }
+    }
+
+    public void drain() {
+        if (!isEmpty() && isBoiled()) {
+            empty = true;
+        }
+    }
+
+    public void boil() {
+        if (!isEmpty() && !isBoiled()) {
+            boiled = true;
+        }
+    }
+
+    public boolean isEmpty() {
+        return empty;
+    }
+
+    public boolean isBoiled() {
+        return boiled;
+    }
+
+}
