454df86599d09cfd5d602dfb560b0e6a466b1436
Boston Hsu <bostonhsu@outlook.com>
2017-06-05 16:07:07 +0800

    创建一个命令对象。

---
+++ src/cn/windssoft/command/Light.java
@@ -0,0 +1,10 @@
+package cn.windssoft.command;
+
+/**
+ * Created by Administrator on 2017/6/5.
+ */
+public class Light {
+    public void on() {
+
+    }
+}
---
+++ src/cn/windssoft/command/LightOnCommand.java
@@ -0,0 +1,17 @@
+package cn.windssoft.command;
+
+/**
+ * Created by Administrator on 2017/6/5.
+ */
+public class LightOnCommand implements Command {
+    Light _light;
+
+    public LightOnCommand(Light light) {
+        _light = light;
+    }
+
+    @Override
+    public void execute() {
+        _light.on();
+    }
+}
