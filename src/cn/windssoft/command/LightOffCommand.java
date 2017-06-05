be9e5432d5494ae5dcde023fe1fde1e095ea1500
Boston Hsu <bostonhsu@outlook.com>
2017-06-05 17:03:31 +0800

    下班，未完待续。

---
+++ src/cn/windssoft/command/LightOffCommand.java
@@ -0,0 +1,17 @@
+package cn.windssoft.command;
+
+/**
+ * Created by Administrator on 2017/6/5.
+ */
+public class LightOffCommand implements Command {
+    Light _light;
+
+    public LightOffCommand(Light light) {
+        _light = light;
+    }
+
+    @Override
+    public void execute() {
+        _light.off();
+    }
+}
