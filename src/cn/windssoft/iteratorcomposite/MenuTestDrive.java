db9b080a1ef529e4c4874f8fdd258e544b97b8cf
Boston Hsu <bostonhsu@outlook.com>
2017-06-09 10:34:12 +0800

    实现两个打印功能。

--- src/cn/windssoft/iteratorcomposite/MenuTestDrive.java
+++ src/cn/windssoft/iteratorcomposite/MenuTestDrive.java
@@ -6,11 +6,25 @@ import java.util.ArrayList;
  * Created by Administrator on 2017/6/9.
  */
 public class MenuTestDrive {
-    PanCakeHouseMenu panCakeHouseMenu = new PanCakeHouseMenu();
-    ArrayList breakfastItems = panCakeHouseMenu.getMenuItems();
+    public static void main(String[] args) {
+        PanCakeHouseMenu panCakeHouseMenu = new PanCakeHouseMenu();
+        ArrayList breakfastItems = panCakeHouseMenu.getMenuItems();
 
-    DinerMenu dinerMenu = new DinerMenu();
-    MenuItem[] lunchItems = dinerMenu.getMenuItems();
+        DinerMenu dinerMenu = new DinerMenu();
+        MenuItem[] lunchItems = dinerMenu.getMenuItems();
 
-    
+        for (int i = 0; i < breakfastItems.size(); i++) {
+            MenuItem menuItem = (MenuItem)breakfastItems.get(i);
+            System.out.print(menuItem.getName() + "\t");
+            System.out.println(menuItem.getRice() + "\t");
+            System.out.println(menuItem.getDescription());
+        }
+
+        for (int i = 0; i < lunchItems.length; i++) {
+            MenuItem menuItem = lunchItems[i];
+            System.out.print(menuItem.getName() + "\t");
+            System.out.println(menuItem.getRice() + "\t");
+            System.out.println(menuItem.getDescription());
+        }
+    }
 }
