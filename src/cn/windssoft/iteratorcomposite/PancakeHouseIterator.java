06c4765e33c217f75dd2e05d8ad4d687744a20a7
Boston Hsu <bostonhsu@outlook.com>
2017-06-09 11:43:20 +0800

    test

--- src/cn/windssoft/iteratorcomposite/PancakeHouseIterator.java
+++ src/cn/windssoft/iteratorcomposite/PancakeHouseIterator.java
@@ -5,9 +5,27 @@ import java.util.ArrayList;
 /**
  * Created by Administrator on 2017/6/9.
  */
-public class PancakeHouseIterator extends DinerMenuIterator {
+public class PancakeHouseIterator implements Iterator {
+    ArrayList menuItems;
+    int position = 0;
 
     public PancakeHouseIterator(ArrayList menuItems) {
-        this.menuItems = (MenuItem[]) menuItems.toArray();
+        this.menuItems = menuItems;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (position >= menuItems.size()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public Object next() {
+        Object object = menuItems.get(position);
+        position ++;
+        return object;
     }
 }
