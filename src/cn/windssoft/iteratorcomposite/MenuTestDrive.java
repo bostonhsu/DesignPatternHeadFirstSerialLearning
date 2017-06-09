8fadb83e0d23b474bf28cd2b8ed20cd269edeee5
Boston Hsu <bostonhsu@outlook.com>
2017-06-09 11:12:19 +0800

    更新了测试代码。

--- src/cn/windssoft/iteratorcomposite/MenuTestDrive.java
+++ src/cn/windssoft/iteratorcomposite/MenuTestDrive.java
@@ -8,23 +8,9 @@ import java.util.ArrayList;
 public class MenuTestDrive {
     public static void main(String[] args) {
         PanCakeHouseMenu panCakeHouseMenu = new PanCakeHouseMenu();
-        ArrayList breakfastItems = panCakeHouseMenu.getMenuItems();
-
         DinerMenu dinerMenu = new DinerMenu();
-        MenuItem[] lunchItems = dinerMenu.getMenuItems();
 
-        for (int i = 0; i < breakfastItems.size(); i++) {
-            MenuItem menuItem = (MenuItem)breakfastItems.get(i);
-            System.out.print(menuItem.getName() + "\t");
-            System.out.println(menuItem.getRice() + "\t");
-            System.out.println(menuItem.getDescription());
-        }
-
-        for (int i = 0; i < lunchItems.length; i++) {
-            MenuItem menuItem = lunchItems[i];
-            System.out.print(menuItem.getName() + "\t");
-            System.out.println(menuItem.getRice() + "\t");
-            System.out.println(menuItem.getDescription());
-        }
+        Waitress waitress = new Waitress(panCakeHouseMenu, dinerMenu);
+        waitress.printMenu();
     }
 }
