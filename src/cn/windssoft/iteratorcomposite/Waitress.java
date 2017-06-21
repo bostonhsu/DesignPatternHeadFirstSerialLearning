c898cbf99b8516763617cddfe7247a3ced4c442d
徐智勇 <bostonhsu@outlook.com>
2017-06-21 16:57:20 +0800

    因为Waitress类在打印方法里需要调用三次打印函数，以及每一次增加菜单项，都需要更改这里。
    所以，把其内部的一个属性：菜单，修改成菜单列表。

--- src/cn/windssoft/iteratorcomposite/Waitress.java
+++ src/cn/windssoft/iteratorcomposite/Waitress.java
@@ -1,5 +1,6 @@
 package cn.windssoft.iteratorcomposite;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 
 /**
@@ -7,26 +8,18 @@ import java.util.Iterator;
  */
 
 public class Waitress {
-    Menu pancakeHouseMenu;
-    Menu dinerMenu;
-    Menu cafeMenu;
+    ArrayList menus;
 
-    public Waitress(Menu panCakeHouseMenu, Menu dinerMenu, Menu cafeMenu) {
-        this.pancakeHouseMenu = panCakeHouseMenu;
-        this.dinerMenu = dinerMenu;
-        this.cafeMenu = cafeMenu;
+    public Waitress(ArrayList menus) {
+        this.menus = menus;
     }
 
     public void printMenu() {
-        Iterator pancakeIterator = pancakeHouseMenu.createIterator();
-        Iterator dinerIterator = dinerMenu.createIterator();
-        Iterator cafeIterator = cafeMenu.createIterator();
-        System.out.println("MENU\n-------\nBREAKFAST");
-        printMenu(pancakeIterator);
-        System.out.println("\nLUNCH");
-        printMenu(dinerIterator);
-        System.out.println("\nCAFE");
-        printMenu(cafeIterator);
+        Iterator menuIterator = menus.iterator();
+        while (menuIterator.hasNext()) {
+            Menu menu = (Menu) menuIterator.next();
+            printMenu(menu.createIterator());
+        }
     }
 
     private void printMenu(Iterator iterator) {
