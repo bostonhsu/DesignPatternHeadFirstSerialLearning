9e980c0db327733633c2db6373a3ed4ca864b419
徐智勇 <bostonhsu@outlook.com>
2017-06-21 16:36:14 +0800

    编写测试。

--- src/cn/windssoft/iteratorcomposite/MenuTestDrive.java
+++ src/cn/windssoft/iteratorcomposite/MenuTestDrive.java
@@ -9,8 +9,9 @@ public class MenuTestDrive {
     public static void main(String[] args) {
         PanCakeHouseMenu panCakeHouseMenu = new PanCakeHouseMenu();
         DinerMenu dinerMenu = new DinerMenu();
+        CafeMenu cafeMenu = new CafeMenu();
 
-        Waitress waitress = new Waitress(panCakeHouseMenu, dinerMenu);
+        Waitress waitress = new Waitress(panCakeHouseMenu, dinerMenu, cafeMenu);
         waitress.printMenu();
     }
 }
