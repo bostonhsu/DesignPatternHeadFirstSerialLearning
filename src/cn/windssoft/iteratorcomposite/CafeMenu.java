4ca95a295c9d40cd12c0f886316b09b9515c1b51
徐智勇 <bostonhsu@outlook.com>
2017-06-21 16:28:15 +0800

    此时，咖啡厅餐厅合并了进来。

---
+++ src/cn/windssoft/iteratorcomposite/CafeMenu.java
@@ -0,0 +1,25 @@
+package cn.windssoft.iteratorcomposite;
+
+import java.util.Hashtable;
+
+/**
+ * Created by Boston Hsu on 6/21/2017.
+ */
+public class CafeMenu {
+    Hashtable menuItems = new Hashtable();
+
+    public CafeMenu() {
+        addItem("Veggie Burge and Air Fries", "Veggie burge on a whole wheat bun, lettuce, tomato, and fries", true, 3.99);
+        addItem("Soup of the day", "A cup of the soup of the day, with a side salad", false, 3.69);
+        addItem("Burrito", "A large burrito, with whole pinto beans, salsa, guacamola", true, 4.29);
+    }
+
+    public void addItem(String name, String description, boolean vegetarian, double price) {
+        MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+        menuItems.put(menuItem.getName(), menuItem);
+    }
+
+    public Hashtable getItems() {
+        return menuItems;
+    }
+}
