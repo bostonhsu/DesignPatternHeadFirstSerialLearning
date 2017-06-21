03c1597b1ca6ed536040696ff89e923bdb9cbc5e
徐智勇 <bostonhsu@outlook.com>
2017-06-21 16:34:46 +0800

    修改Waitress类。

--- src/cn/windssoft/iteratorcomposite/Waitress.java
+++ src/cn/windssoft/iteratorcomposite/Waitress.java
@@ -9,20 +9,24 @@ import java.util.Iterator;
 public class Waitress {
     Menu pancakeHouseMenu;
     Menu dinerMenu;
+    Menu cafeMenu;
 
-
-    public Waitress(Menu panCakeHouseMenu, Menu dinerMenu) {
+    public Waitress(Menu panCakeHouseMenu, Menu dinerMenu, Menu cafeMenu) {
         this.pancakeHouseMenu = panCakeHouseMenu;
         this.dinerMenu = dinerMenu;
+        this.cafeMenu = cafeMenu;
     }
 
     public void printMenu() {
         Iterator pancakeIterator = pancakeHouseMenu.createIterator();
         Iterator dinerIterator = dinerMenu.createIterator();
+        Iterator cafeIterator = cafeMenu.createIterator();
         System.out.println("MENU\n-------\nBREAKFAST");
         printMenu(pancakeIterator);
         System.out.println("\nLUNCH");
         printMenu(dinerIterator);
+        System.out.println("\nCAFE");
+        printMenu(cafeIterator);
     }
 
     private void printMenu(Iterator iterator) {
