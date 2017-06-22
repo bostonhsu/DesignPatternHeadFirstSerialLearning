00de639627aff3bc86e67b685ce715823b024ed1
Boston Hsu <bostonhsu@outlook.com>
2017-06-22 15:34:34 +0800

    构建了一个PersonBean类。
    但是，有人提出要：不能让任何人修改自己的评分。
    这里的解决方法是：使用保护代理。

---
+++ src/cn/windssoft/proxy/PersonBean.java
@@ -0,0 +1,16 @@
+package cn.windssoft.proxy;
+
+/**
+ * Created by Administrator on 2017/6/22.
+ */
+public interface PersonBean {
+    String getName();
+    String getGender();
+    String getInterests();
+    int getHotOrNotRating();
+
+    void setName(String name);
+    void setGender(String gender);
+    void setInterests(String interests);
+    void setHotOrNotRating(int rating);
+}
