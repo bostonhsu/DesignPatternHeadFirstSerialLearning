15fca9a274ff6f6a895ff37af47d2808afec45ba
Boston Hsu <bostonhsu@outlook.com>
2017-06-22 16:33:23 +0800

    创建了工厂体系，用来生产统一标配的鸭子。

---
+++ src/cn/windssoft/compound/AbstractDuckFactory.java
@@ -0,0 +1,11 @@
+package cn.windssoft.compound;
+
+/**
+ * Created by Administrator on 2017/6/22.
+ */
+public abstract class AbstractDuckFactory {
+    public abstract Quackable createMallardDuck();
+    public abstract Quackable createRedheadDuck();
+    public abstract Quackable createDuckCall();
+    public abstract Quackable createRubberDuck();
+}
---
+++ src/cn/windssoft/compound/DuckFactory.java
@@ -0,0 +1,26 @@
+package cn.windssoft.compound;
+
+/**
+ * Created by Administrator on 2017/6/22.
+ */
+public class DuckFactory extends AbstractDuckFactory {
+    @Override
+    public Quackable createMallardDuck() {
+        return new MallardDuck();
+    }
+
+    @Override
+    public Quackable createRedheadDuck() {
+        return new RedheadDuck();
+    }
+
+    @Override
+    public Quackable createDuckCall() {
+        return new DuckCall();
+    }
+
+    @Override
+    public Quackable createRubberDuck() {
+        return new RubberDuck();
+    }
+}
