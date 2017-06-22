17625b7afb3e9e18ce298f62cf926911ea3abf0f
Boston Hsu <bostonhsu@outlook.com>
2017-06-22 16:59:47 +0800

    然后让每一只可以呱呱叫的家伙都要实现可以被观察到。

--- src/cn/windssoft/compound/Quackable.java
+++ src/cn/windssoft/compound/Quackable.java
@@ -3,6 +3,6 @@ package cn.windssoft.compound;
 /**
  * Created by Administrator on 2017/6/22.
  */
-public interface Quackable {
+public interface Quackable extends QuackObservable{
     public void quack();
 }
