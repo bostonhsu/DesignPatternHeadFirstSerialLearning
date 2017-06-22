a0e4415001d840e85eb9210ddf9594d89d5e9b58
Boston Hsu <bostonhsu@outlook.com>
2017-06-22 14:12:55 +0800

    实现了状态模式。

--- src/cn/windssoft/statePattern/GumballMachine.java
+++ src/cn/windssoft/statePattern/GumballMachine.java
@@ -11,4 +11,59 @@ public class GumballMachine {
 
     State state = soldOutState;
     int count = 0;
+
+    public GumballMachine(int numberGumballs) {
+        soldOutState = new SoldOutState(this);
+        noQuarterState = new NoQuarterState(this);
+        hasQuarterState = new HasQuarterState(this);
+        soldState = new SoldState(this);
+        this.count = numberGumballs;
+        if (numberGumballs > 0) {
+            state = noQuarterState;
+        }
+    }
+
+    public void insertQuarter() {
+        state.insertQuarter();
+    }
+
+    public void ejectQuarter() {
+        state.ejectQuarter();
+    }
+
+    public void turnCrank() {
+        state.turnCrank();
+    }
+
+    void setState(State state) {
+        this.state = state;
+    }
+
+    void releaseBall() {
+        System.out.println("A gumball comes rolling out the slot...");
+        if (count != 0) {
+            count--;
+        }
+    }
+
+    public State getHasQuarterState() {
+        return this.hasQuarterState;
+    }
+
+    public State getSoldOutState() {
+        return soldOutState;
+    }
+
+    public State getNoQuarterState() {
+        return noQuarterState;
+    }
+
+    public State getSoldState() {
+        return soldState;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
 }
---
+++ src/cn/windssoft/statePattern/SoldOutState.java
@@ -0,0 +1,50 @@
+package cn.windssoft.statePattern;
+
+/**
+ * Created by Administrator on 2017/6/22.
+ */
+public class SoldOutState implements State {
+    GumballMachine gumballMachine;
+
+    public SoldOutState(GumballMachine gumballMachine) {
+        this.gumballMachine = gumballMachine;
+    }
+
+    @Override
+    public void insertQuarter() {
+        System.out.println("There isn't any gumball");
+    }
+
+    @Override
+    public void ejectQuarter() {
+        System.out.println("You haven't inserted a quarter");
+    }
+
+    @Override
+    public void turnCrank() {
+        System.out.println("You haven't inserted a quarter");
+    }
+
+    @Override
+    public void dispense() {
+        System.out.println("There isn't any gumball");
+    }
+
+    public static void main(String[] args) {
+        char c = 'A';
+        int num = 10;
+        switch (c) {
+            case 'B':
+                num ++;
+            case 'A':
+                num ++;
+            case 'Y':
+                num ++;
+                break;
+                default:
+                    num --;
+        }
+
+        System.out.println(num);
+    }
+}
