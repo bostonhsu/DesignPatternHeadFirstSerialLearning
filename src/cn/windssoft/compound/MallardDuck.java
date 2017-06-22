2109435400b514bb9e3e6b15d7739d61c535236f
Boston Hsu <bostonhsu@outlook.com>
2017-06-22 17:15:07 +0800

    正在写关于观察者模式，四爷让下班。

--- src/cn/windssoft/compound/MallardDuck.java
+++ src/cn/windssoft/compound/MallardDuck.java
@@ -1,11 +1,30 @@
 package cn.windssoft.compound;
 
+import java.util.Observer;
+
 /**
  * Created by Administrator on 2017/6/22.
  */
 public class MallardDuck implements Quackable {
+    Observable observable;
+
+    public MallardDuck() {
+        this.observable = new Observable(this);
+    }
+
     @Override
     public void quack() {
         System.out.println("Quack");
+        notifyObservers();
+    }
+
+    @Override
+    public void registerObserver(Observer observer) {
+        observable.registerObserver(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+        observable.notifyObservers();
     }
 }
--- src/cn/windssoft/compound/RedheadDuck.java
+++ src/cn/windssoft/compound/RedheadDuck.java
@@ -1,11 +1,32 @@
 package cn.windssoft.compound;
 
+import com.sun.org.apache.regexp.internal.RE;
+
+import java.util.Observer;
+
 /**
  * Created by Administrator on 2017/6/22.
  */
 public class RedheadDuck implements Quackable {
+    Observable observable;
+
+    public RedheadDuck() {
+        this.observable = new Observable(this);
+    }
+
     @Override
     public void quack() {
         System.out.println("Quack");
+        observable.notifyObservers();
+    }
+
+    @Override
+    public void registerObserver(Observer observer) {
+        observable.registerObserver(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+        observable.notifyObservers();
     }
 }
