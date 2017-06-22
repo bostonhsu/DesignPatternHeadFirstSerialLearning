2e2ccdc106635955b98d47ea22a3cfb79dc2522f
Boston Hsu <bostonhsu@outlook.com>
2017-06-22 16:36:49 +0800

    修改一下实验场，在里面修建一个生产鸭子的工厂，让所有的鸭子都经过工厂的加工。

--- src/cn/windssoft/compound/DuckSimulator.java
+++ src/cn/windssoft/compound/DuckSimulator.java
@@ -4,14 +4,14 @@ package cn.windssoft.compound;
  * Created by Administrator on 2017/6/22.
  */
 public class DuckSimulator {
-    private void simulate() {
-        Quackable mallardDuck = new QuackCounter(new MallardDuck());
-        Quackable redheadDuck = new QuackCounter(new RedheadDuck());
-        Quackable duckCall = new QuackCounter(new DuckCall());
-        Quackable rubberDuck = new QuackCounter(new RubberDuck());
+    private void simulate(AbstractDuckFactory duckFactory) {
+        Quackable mallardDuck = duckFactory.createMallardDuck();
+        Quackable redheadDuck = duckFactory.createRedheadDuck();
+        Quackable duckCall = duckFactory.createDuckCall();
+        Quackable rubberDuck = duckFactory.createRubberDuck();
         Quackable goose = new GooseAdapter(new Goose());
 
-        System.out.println("Duck Simulator: with Decorator");
+        System.out.println("Duck Simulator: with Abstract Factory");
 
         simulate(mallardDuck);
         simulate(redheadDuck);
@@ -28,6 +28,7 @@ public class DuckSimulator {
 
     public static void main(String[] args) {
         DuckSimulator duckSimulator = new DuckSimulator();
-        duckSimulator.simulate();
+        AbstractDuckFactory duckFactory = new CountingDuckFactory();
+        duckSimulator.simulate(duckFactory);
     }
 }
