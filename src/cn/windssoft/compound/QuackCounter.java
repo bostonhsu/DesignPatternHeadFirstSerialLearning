c162d32965cc24268ff11dde7bd322d844f56ab4
Boston Hsu <bostonhsu@outlook.com>
2017-06-23 10:07:25 +0800

    完成了所有的Observable端的工作。现在开始完成Observer端的。

--- src/cn/windssoft/compound/QuackCounter.java
+++ src/cn/windssoft/compound/QuackCounter.java
@@ -1,23 +1,38 @@
 package cn.windssoft.compound;
 
+import java.util.Observer;
+
 /**
  * Created by Administrator on 2017/6/22.
  */
 public class QuackCounter implements Quackable {
     Quackable duck;
     static int numberOfQuacks;
+    Observable observable;
 
     public QuackCounter(Quackable duck) {
         this.duck = duck;
+        this.observable = new Observable(this);
     }
 
     @Override
     public void quack() {
         duck.quack();
+        notifyObservers();
         numberOfQuacks ++;
     }
 
     public static int getNumberOfQuacks() {
         return numberOfQuacks;
+    }
+
+    @Override
+    public void registerObserver(Observer observer) {
+        observable.registerObserver(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+        observable.notifyObservers();
     }
 }
